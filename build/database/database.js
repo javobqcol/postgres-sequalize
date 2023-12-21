b8ac5a32add9e310dce41295beeb31c887ad255e
javobqcol <javobqcol@yahoo.es>
2023-12-21 17:52:38 -0500

    Delete build directory

--- build/database/database.js
+++
@@ -1,5 +0,0 @@
-import { Sequelize } from "sequelize";
-export const sequalize = new Sequelize("proyectsdb", "admin", "postgres", {
-  host: "localhost",
-  dialect: "postgres"
-});
--- build/routes/task.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createTask, deleteTask, getTask, getTasks, updateTask } from "../controllers/task.controller.js";
-export const router = Router();
-router.get("/tasks", getTasks);
-router.post("/tasks", createTask);
-router.put("/tasks/:id", updateTask);
-router.delete("/tasks/:id", deleteTask);
-router.get("/tasks/:id", getTask);
